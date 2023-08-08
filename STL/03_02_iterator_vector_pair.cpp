cf72967d223b92de6322c30cbe3ff39fed1eb081
Deepanshu <117862458+deepanshu0304@users.noreply.github.com>
2023-08-08 15:23:13 +0530

    Update 03_02_iterator_vector_pair.cpp

--- STL/03_02_iterator_vector_pair.cpp
+++ STL/03_02_iterator_vector_pair.cpp
@@ -7,11 +7,12 @@ int main()
 
     vector<pair<int, int>>::iterator it = v.begin();
 
-    /*upper line is syntax to declare  that it is iterator
+    /*
+    Above line is syntax to declare  that it is iterator
     having name it and of type vector<pair<int, int>>
-    so it can be shortened by using auto keyword
+    It can be shortened by using auto keyword
     */
-    cout << it->first << endl;
+    cout << it->first << endl; // This is arrow operator which is short-hand for (*it).first;
     cout << ++it->second << endl;
     cout << (*++it).second << endl;
     for (it = v.begin(); it != v.end(); it++)
