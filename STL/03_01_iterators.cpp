ccfff88ea265841558fbff6b1467cdfba7d8354a
Deepanshu <117862458+deepanshu0304@users.noreply.github.com>
2023-08-08 15:15:10 +0530

    Update 03_01_iterators.cpp

--- STL/03_01_iterators.cpp
+++ STL/03_01_iterators.cpp
@@ -7,13 +7,31 @@ int main(){
         cout<<v[i]<<" ";
     }
     cout<<endl;
-    vector<int> :: iterator it = v.begin() ;
+    /*
+    Syntax of declaring a iterators.
+    [data_structure_name] :: iterators [any_name] ;
+    there are two methods begin and end ...begin point to the first element of data structure 
+    and end point to the address which is just after the end of data structure
+    */
+    vector<int> :: iterator it = v.begin() ; // we declared iterator which is pointing to first element of vector....
     cout<<*it<<endl;
-    cout<<*(it+1)<<endl; // down two method are also correct way of rwritin
-    // cout<<*(++it)<<endl;
-    // cout<<*++it<<endl;
-    for(it=v.begin(); it !=v.end();it++){
+    cout<<*(it+1)<<endl; // below two method are also correct way of writing
+    cout<<*(++it)<<endl;
+    cout<<*++it<<endl;
+    for(it=v.begin(); it !=v.end();it++){   // iterator doesn't make lot of sense in vectors because it is linear data structure so we can access it 
+        // using indexes but iterator is imp in case of maps and sets...
         cout<<*it<<endl;
     }
     return 0;
 }
+
+/*
+Important concept 
+suppose there is variable named a.
+i.) a=a+1;
+ii.) a++;
+Here meaning of i.) and ii.) are same they will do same operation
+but in case of iterators these two are different things (except vectors)..
+if we do it = it+1 ....it will take 
+
+*/
