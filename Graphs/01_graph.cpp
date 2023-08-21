d81f1933ebdbb80f0a94d422fee18c3037d596b8
Deepanshu <117862458+deepanshu0304@users.noreply.github.com>
2023-08-22 01:30:33 +0530

    Update 01_graph.cpp

--- Graphs/01_graph.cpp
+++ Graphs/01_graph.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 /*
 A graph that has no cyclic structure in it is basically a tree ....
-In tree the no of edges will always be nodes -1 
+In tree the no of edges will always be one less than number of edges
 
-Connected component ---> A component in gaph thorugh which you can traverse to all the vertex in that component
-these diffrent component are called  connected component 
+Connected component ---> A component in gaph through which you can traverse to all the vertex in that component
+These diffrent component are called  connected component 
 
-There are directed graph in which oyu can travers only in specific direction in edges....
-now it can be cyclic and acyclic ...Non directed acyclic graph are simply called tree
+There are directed graph in which you can traverse only in specific direction....
+Now it can be cyclic or acyclic ...Non directed acyclic graph are simply a tree
 
 In case of directed graph we call it strongly connected component... A component in which you caan move to
 each and every vertex of that component is strongly connected cmponent
 
 Manytime a whole graph who have some component together is known as forest
 
-Given a tree ....it's any part taken that also represent a tree is known a subtree 
+In a tree ,  any part that is also tree is known a subtree 
 
-Leaf node is that node which do not have any child
+Nodes that doesn't have any childs are called Leaf Nodes
 
 Depth ---> depth of node is defined as distance from root node
 Height---> height of node is defined as longest path from any leaf node .....and we can only go up 
